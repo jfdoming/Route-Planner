c996e9d276e9541e2a8a336ec92a6157f1b526fe
Julian Frank Dominguez-Schatz <dolphinspray.fr@gmail.com>
2017-05-07 15:24:33 -0400

    Added placeholder classes.

---
+++ src/com/idkwhattoputhere/routeplanner/main/RenderingLoop.java
@@ -0,0 +1,29 @@
+package com.idkwhattoputhere.routeplanner.main;
+
+/**
+ * Assignment: Route Planner
+ * Author: Julian Dominguez-Schatz
+ * Date: 07/05/2017
+ * Description: Represents the rendering loop in our application.
+ */
+public class RenderingLoop extends LoopingThread {
+
+    public RenderingLoop() {
+        super("rendering-loop");
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    protected void prepare() {
+        super.prepare();
+    }
+
+    @Override
+    protected void dispose() {
+        super.dispose();
+    }
+}
---
+++ src/com/idkwhattoputhere/routeplanner/main/UpdatingLoop.java
@@ -0,0 +1,29 @@
+package com.idkwhattoputhere.routeplanner.main;
+
+/**
+ * Assignment: Route Planner
+ * Author: Julian Dominguez-Schatz
+ * Date: 07/05/2017
+ * Description: Represents the updating loop in our application.
+ */
+public class UpdatingLoop extends LoopingThread {
+
+    public UpdatingLoop() {
+        super("updating-loop");
+    }
+
+    @Override
+    protected void update() {
+
+    }
+
+    @Override
+    protected void prepare() {
+        super.prepare();
+    }
+
+    @Override
+    protected void dispose() {
+        super.dispose();
+    }
+}
