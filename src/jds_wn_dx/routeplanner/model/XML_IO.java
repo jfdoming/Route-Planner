0a936496f929ff30ae086131ef7e6139d6e2bf76
Danny Xu <valuemeta@gmail.com>
2017-05-30 00:27:41 +0000

    Update XML to be worse

--- src/jds_wn_dx/routeplanner/model/XML_IO.java
+++ src/jds_wn_dx/routeplanner/model/XML_IO.java
@@ -4,21 +4,28 @@ import org.w3c.dom.*;
 
 import javax.xml.parsers.*;
 import java.io.File;
+import java.util.*;
 
 public class XML_IO {
+
+    private File file;
+    private DocumentBuilderFactory dbFactory;
+    private DocumentBuilder dBuilder;
+    private Document doc;
+    private NodeList routeTypeList;
+    private NodeList startList;
+    private NodeList endList;
+    private Node node;
+    private Element element;
+    private ArrayList<String> routeType = new ArrayList<String>(0);
+    private ArrayList<String> start = new ArrayList<String>(0);
+    private ArrayList<String> end = new ArrayList<String>(0);
+
+    private final String encoding = "UTF-8";
 
     public XML_IO() {
         initialize();
     }
-
-    File file;
-    DocumentBuilderFactory dbFactory;
-    DocumentBuilder dBuilder;
-    Document doc;
-    NodeList pathList;
-    Node meme;
-    Element dank;
-    private final String encoding = "UTF-8";
 
     private void initialize() {
         try {
@@ -32,12 +39,28 @@ public class XML_IO {
 
     public void getData() {
         try {
-            pathList = doc.getElementsByTagName("RouteType");
-            for (int i = 0; i < pathList.getLength(); i++) {
-                meme = pathList.item(i);
-                if (meme.getNodeType() == Node.ELEMENT_NODE) {
-                    dank = (Element) meme;
-                    System.out.println(dank.getTextContent());
+            routeTypeList = doc.getElementsByTagName("RouteType");
+            for (int i = 0; i < routeTypeList.getLength(); i++) {
+                node = routeTypeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    element = (Element) node;
+                    routeType.add(i, element.getTextContent());
+                }
+            }
+            startList = doc.getElementsByTagName("Start");
+            for (int i = 0; i < startList.getLength(); i++) {
+                node = startList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    element = (Element) node;
+                    start.add(i, element.getTextContent());
+                }
+            }
+            endList = doc.getElementsByTagName("End");
+            for (int i = 0; i < endList.getLength(); i++) {
+                node = endList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    element = (Element) node;
+                    end.add(i, element.getTextContent());
                 }
             }
         } catch (Exception e) {
@@ -55,7 +78,16 @@ public class XML_IO {
         }
     }
 
+    public ArrayList<String> getRouteType() {
+        return routeType;
+    }
 
+    public ArrayList<String> getStart() {
+        return start;
+    }
 
+    public ArrayList<String> getEnd() {
+        return end;
+    }
 
 }
